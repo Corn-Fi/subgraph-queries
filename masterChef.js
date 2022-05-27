c94094fdc7926d41dcb1204bce2678e2812ed8c1
Korey Corkscrew <77598913+korey-corkscrew@users.noreply.github.com>
2022-05-27 09:36:01 -0500

    subgraphs

---
+++ masterChef.js
@@ -0,0 +1,43 @@
+const { request, gql } = require('graphql-request');
+
+const API_URL = 'https://api.thegraph.com/subgraphs/name/kcorkscrew/polygon'
+
+async function getUserPoolAmounts(user) {
+    const result = await request(API_URL,
+        gql`{
+                pools(orderBy:id) {
+                    id
+                    users(where: {address:"${user}"}){
+                        amount
+                    }
+                }
+            }`
+    );
+    return result
+}
+
+async function getPoolAmounts() {
+    const result = await request(API_URL,
+        gql`{
+                pools(orderBy: id) {
+                    id
+                    pair
+                    balance
+                }
+            }`
+    );
+    return result
+}
+
+async function main() {
+    const userPoolAmounts = await getUserPoolAmounts("0x79297c94d382cca69af6ca3782907d7a461f9031");
+    const poolAmounts = await getPoolAmounts()
+    console.log(poolAmounts)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
